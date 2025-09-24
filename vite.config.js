cc16eb423e3ec174e25f74014d56d5b714f3c02b
bhargav.guggilapu <bhargavguggilapu@gmail.com>
2025-09-24 10:14:30 -0400

    revert changes

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/insights": {
-        target: "http://4.187.210.124.nip.io",
-        changeOrigin: true,
-        rewrite: (p) => p.replace(/^\/api\/insights/, "/insights"),
-      },
-    },
-  },
 });
